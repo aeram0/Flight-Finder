40730bf2c1a409b4c5d396d8cb477b1ae55da8e9
Julia Bighetto <bighetto@bu.edu>
2020-03-31 09:22:56 -0400

    Logo v1

--- code/currencies.py
+++ code/currencies.py
@@ -33,11 +33,3 @@ def finding_currency(location, destination):
 
 
 #print(type(finding_currency("US", "Spain")))
-
-def backend(country, location, weather, inbound, outbound, people, budget):
-    # right now country is only one. We have to do something to change it into multiple countries
-    crr = finding_currency(location, country)
-    actual_budget = crr * int(budget)
-    return crr, int(actual_budget)
-
-print(backend("US", "Spain", "", "", "", "", "3500"))
